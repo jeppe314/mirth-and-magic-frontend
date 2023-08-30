46ee5b7036a610e7352eeb4d7db288da65cce9c8
jeppe314 <jesperlindqvist93@gmail.com>
2023-08-30 23:15:24 +0200

    Basic router init

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -13,7 +21,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         redirect_uri: window.location.origin,
       }}
     >
-      <App />
+      <RouterProvider router={router} />
     </Auth0Provider>
   </React.StrictMode>
 );
