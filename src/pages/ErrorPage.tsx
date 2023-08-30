0b548777ce4599ad3b4e439ee43d8aef010935f9
jeppe314 <jesperlindqvist93@gmail.com>
2023-08-30 23:27:07 +0200

    Added useRouteError to errorpage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useRouteError } from "react-router-dom";
 
 type Props = {};
 
 export default function ErrorPage({}: Props) {
+  const error = useRouteError();
+  console.error(error);
   return (
     <div className="bg-slate-500 h-screen">
       <div className="text-slate-100">Oh noezz!! I can't find the page you're looking for :((</div>
