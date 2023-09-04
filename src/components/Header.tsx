b0661e81ff5235ab6d4d786e1fdbd97528f0293b
jeppe314 <jesperlindqvist93@gmail.com>
2023-09-04 20:46:21 +0200

    Made the component lol

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export default function Header({}: Props) {
+  return (
+    <div className="h-16 w-full flex justify-center items-center">
+      <div className="text-xl">Mirth & Magic</div>
+    </div>
+  );
+}
