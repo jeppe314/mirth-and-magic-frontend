ca21f8863175a5385db08011fd92ed208c6e82e8
jeppe314 <jesperlindqvist93@gmail.com>
2023-09-07 15:44:54 +0200

    Commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,13 @@
-import BottomNavigation from "../components/BottomNavigation";
-import Header from "../components/Header";
 import LogoutButton from "../components/LogoutButton";
 import { useAuth0 } from "@auth0/auth0-react";
 type Props = {};
 
 export default function Home({}: Props) {
-  const { user, isAuthenticated } = useAuth0();
+  const { user } = useAuth0();
+  console.log("🚀 ~ file: Home.tsx:9 ~ Home ~ user:", user);
   return (
     <div className="h-full flex flex-col justify-between items-center font-sans">
-      {isAuthenticated}
-      {user?.name}
+      {JSON.stringify(user)}
       hoome
       <LogoutButton />
     </div>
