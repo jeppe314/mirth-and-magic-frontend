cfd9906367520de3bb3fdb22af4448a80b1f3d69
jeppe314 <jesperlindqvist93@gmail.com>
2023-09-08 18:07:36 +0200

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
 import { authInterceptor } from "./interceptors/interceptors";
 
 function AuthInject() {
-  const { user, getAccessTokenSilently } = useAuth0();
+  const { getAccessTokenSilently } = useAuth0();
 
   useEffect(() => {
     authInterceptor.setAuthGetter(getAccessTokenSilently);
